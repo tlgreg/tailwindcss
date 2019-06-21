3db417db8c564de8966a8f4259d32bcd540229fd
Gergely Tarján <666791+tlgreg@users.noreply.github.com>
2019-06-21 23:50:05 +0200

    Replace request.resolve with resolve.sync

--- src/plugins/preflight.js
+++ src/plugins/preflight.js
@@ -1,9 +1,10 @@
 import fs from 'fs'
 import postcss from 'postcss'
+import resolve from 'resolve'
 
 export default function() {
   return function({ addBase }) {
-    const normalizeStyles = postcss.parse(fs.readFileSync(require.resolve('normalize.css'), 'utf8'))
+    const normalizeStyles = postcss.parse(fs.readFileSync(resolve.sync('normalize.css'), 'utf8'))
     const preflightStyles = postcss.parse(fs.readFileSync(`${__dirname}/css/preflight.css`, 'utf8'))
     addBase([...normalizeStyles.nodes, ...preflightStyles.nodes])
   }
